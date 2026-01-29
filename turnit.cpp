9a7e60c313ba91028d3375cf210cfbaf5cd6ad87
Orphieus <yatharthnehradpspune@gmail.com>
2026-01-29 14:19:18 +0530

    Difficulty 1350

---
+++ turnit.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int T;
+    cin >> T;
+    while(T-- != 0){
+        int U, V, A, S;
+        cin >> U;
+        cin >> V;
+        cin >> A;
+        cin >> S;
+
+        int v = U * U  - (2 * A * S);
+        if(V * V <= v){
+            cout << "YES" << endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }
+    }
+}
