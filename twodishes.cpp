7593095ba5a4797b9cbe5f5b0eb100433b0fb791
Orphieus <yatharthnehradpspune@gmail.com>
2026-01-29 13:01:53 +0530

    Difficulty 1254

--- twodishes.cpp
+++ twodishes.cpp
@@ -2,5 +2,32 @@
 using namespace std;
 
 int main(){
-    
+    int T;
+    cin >> T;
+    while(T-- != 0){
+        int N, S;
+        int ma = INT_MIN;
+        if(N >= S){
+            for(int i = 0; i < S + 1; i++){
+                for(int j = S; j >= 0; j--){
+                    if(i + j == S){
+                        ma = max(ma, abs(i - j));
+                    }
+                }
+            }
+
+            cout << ma << endl;
+        }
+        else{
+            for(int i = 0; i < N + 1; i++){
+                for(int j = N; j >= 0; j--){
+                    if(i + j == S){
+                        ma = max(ma, abs(i - j));
+                    }
+                }
+            }
+            cout << ma << endl;
+        }
+
+    }
 }
